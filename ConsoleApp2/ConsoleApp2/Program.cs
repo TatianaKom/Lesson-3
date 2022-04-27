399da049595c1ecb0f2b2f44bc874d5f81932972
Tatiana Komarova <tat.komaro2002@yandex.ru>
2022-04-27 17:54:05 +0700

    initial commit

---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace Lesson3;
+public class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine("Please, enter the first number: ");
+        int firstnumber = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Please, enter the second number: ");
+        int secondnumber = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Please, enter the operation: '+', '-', '*', '/' ");
+        switch (Console.ReadLine())
+        {
+            case "+":
+                Console.WriteLine(firstnumber + secondnumber);
+                break;
+            case "-":
+                Console.WriteLine(firstnumber - secondnumber);
+                break;
+            case "*":
+                Console.WriteLine(firstnumber * secondnumber);
+                break ;
+            case "/":
+                if (secondnumber == 0)
+                {
+                    Console.WriteLine("Error, try again (div/0!)");
+                }
+                else
+                {
+                    Console.WriteLine(firstnumber / secondnumber);
+                }
+
+            break;
+        }
+
+    }
+}
